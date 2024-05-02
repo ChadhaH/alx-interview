839addb19bed17070b12b536bf0617361dee6a98
ChadhaH <chadha.hamed1@gmail.com>
2024-05-02 23:54:23 +0000

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,21 +13,21 @@ i = 0
 
 try:
     for row in sys.stdin:
-    for row_l = row.split(" ")
-    if len(row_l) > 4:
-        cd = row_l[-2]
-	size = int(row_l[-1])
-	if cd in code_stats.keys():
-	    code_stats[cd] += 1
-	sizes += size
-	i += 1
+	row_l = row.split(" ")
+	if len(row_l) > 4:
+            cd = row_l[-2]
+	    size = int(row_l[-1])
+	    if cd in code_stats.keys():
+	        code_stats[cd] += 1
+	    sizes += size
+	    i += 1
 
-    if i == 10:
-        i = 0
-	print('File size: {}'.format(sizes))
-	for k, v in sorted(code_stats.items()):
-	    if v != 0:
-		print('{}: {}'.format(k, v))
+        if i == 10:
+            i = 0
+	    print('File size: {}'.format(sizes))
+	    for k, v in sorted(code_stats.items()):
+	        if v != 0:
+		    print('{}: {}'.format(k, v))
 
 except Exception as error:
     pass
