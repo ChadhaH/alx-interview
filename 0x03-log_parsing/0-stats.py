49e5acbc9ea6abf8fe4d42a7aa9dfcf3c9f5f240
ChadhaH <chadha.hamed1@gmail.com>
2024-05-03 00:01:48 +0000

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
-"""
-	 reads stdin line by line and computes metrics
-"""
+'''a script that reads stdin row by row and computes metrics'''
 
 
 import sys
 
-code_stats = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
+cd_stats = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 sizes = 0
 i = 0
 
 try:
     for row in sys.stdin:
-	row_l = row.split(" ")
-	if len(row_l) > 4:
-            cd = row_l[-2]
-	    size = int(row_l[-1])
-	    if cd in code_stats.keys():
-	        code_stats[cd] += 1
-	    sizes += size
-	    i += 1
+        row_list = row.split(" ")
+        if len(row_list) > 4:
+            cd = row_list[-2]
+            size = int(row_list[-1])
+            if cd in cd_stats.keys():
+                cd_stats[cd] += 1
+            sizes += size
+            i += 1
 
         if i == 10:
             i = 0
-	    print('File size: {}'.format(sizes))
-	    for k, v in sorted(code_stats.items()):
-	        if v != 0:
-		    print('{}: {}'.format(k, v))
+            print('File size: {}'.format(sizes))
+            for k, v in sorted(cd_stats.items()):
+                if v != 0:
+                    print('{}: {}'.format(k, v))
 
-except Exception as error:
+except Exception as err:
     pass
 
 finally:
     print('File size: {}'.format(sizes))
-    for k, v in sorted(code_stats.items()):
-	if v != 0:
-	    print('{}: {}'.format(k,v))
+    for k, v in sorted(cd_stats.items()):
+        if v != 0:
+            print('{}: {}'.format(k, v))
